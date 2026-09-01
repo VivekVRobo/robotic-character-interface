518c1b9eb2877f14072f14d2cbde2871835e7a9d
Vivek Vala <vivekvala562@gmail.com>
2026-09-01 20:37:44 +0530

    PR-013 export RobotGateway boundary

--- src/rci/hardware/__init__.py
+++ src/rci/hardware/__init__.py
@@ -2,12 +2,26 @@
 
 from rci.hardware.memory_transport import MemoryByteTransport, create_memory_transport_pair
 from rci.hardware.protocol_transport import ProtocolTransport
+from rci.hardware.robot_gateway import (
+    GatewayReceipt,
+    GatewaySnapshot,
+    RobotGateway,
+    RobotGatewayError,
+    RobotGatewayProtocolError,
+    RobotGatewayRejected,
+)
 from rci.hardware.transport import AsyncByteTransport, TransportStats
 
 __all__ = [
     "AsyncByteTransport",
+    "GatewayReceipt",
+    "GatewaySnapshot",
     "MemoryByteTransport",
     "ProtocolTransport",
+    "RobotGateway",
+    "RobotGatewayError",
+    "RobotGatewayProtocolError",
+    "RobotGatewayRejected",
     "TransportStats",
     "create_memory_transport_pair",
 ]
