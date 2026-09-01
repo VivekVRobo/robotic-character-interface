7b76508f6de7437cfdf8801cb0557c269e880102
Vivek Vala <vivekvala562@gmail.com>
2026-09-02 00:52:42 +0530

    PR-020 export robot telemetry codecs

--- src/rci/protocols/__init__.py
+++ src/rci/protocols/__init__.py
@@ -3,11 +3,14 @@
 from rci.protocols.checksums import crc16_ccitt_false
 from rci.protocols.constants import PROTOCOL_VERSION, MessageType
 from rci.protocols.framing import Frame, decode_frame, encode_frame
+from rci.protocols.messages import RobotJointTelemetry, RobotTelemetry
 
 __all__ = [
     "Frame",
     "MessageType",
     "PROTOCOL_VERSION",
+    "RobotJointTelemetry",
+    "RobotTelemetry",
     "crc16_ccitt_false",
     "decode_frame",
     "encode_frame",
