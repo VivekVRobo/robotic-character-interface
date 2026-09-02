536d6d535288d2832f794ba28ac98f8e628ac62c
Vivek Vala <vivekvala562@gmail.com>
2026-09-02 12:00:24 +0530

    Export simulation behavior embodiment planner

--- src/rci/behavior/__init__.py
+++ src/rci/behavior/__init__.py
@@ -1,5 +1,11 @@
 """Semantic character behavior planning."""
 
+from rci.behavior.embodiment import EmbodiedBehaviorGoal, SimulationBehaviorEmbodimentPlanner
 from rci.behavior.planner import BehaviorIntent, BehaviorPlanner
 
-__all__ = ["BehaviorIntent", "BehaviorPlanner"]
+__all__ = [
+    "BehaviorIntent",
+    "BehaviorPlanner",
+    "EmbodiedBehaviorGoal",
+    "SimulationBehaviorEmbodimentPlanner",
+]
